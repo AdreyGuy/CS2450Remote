4f9dbb6d71920d53bf22de04661f75bda4f4d132
AdreyGuy <AdreyGuy@DESKTOP-GC6LAH1.localdomain>
2026-01-22 09:36:45 -0700

    first commit

---
+++ git_prg.py
@@ -0,0 +1,17 @@
+def guess_age():
+    int inp
+    inp = input("Greetings, I will try to guess your age. Please, type your name: ")
+    for i in range(15, 30):
+        str inpu = input("Are you" + i + "?")
+        if inpu == y:
+            print(inp + "is" + inpu)
+            return
+        else:
+            print("Rats")
+        return
+guess_age()
+        
+        
+        
+        
+    
